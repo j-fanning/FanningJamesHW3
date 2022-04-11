6a9ac0b1dc5a33eb3e3e1a3bf8d9df91c6c9c14b
James Fanning <fanning07@cox.net>
2022-04-11 02:52:03 -0500

    start vite app

---
+++ src/App.vue
@@ -0,0 +1,23 @@
+<script>
+import MainPage from './views/MainPage.vue'
+export default {
+  components: {
+    MainPage,
+  },
+};
+</script>
+
+<template>
+  <MainPage></MainPage>
+</template>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
